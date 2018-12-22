8a3f02b21a35c5536cb68e0f2186975f23efcbfd
Andrew Gerst <gerst20051@gmail.com>
2018-12-22 03:58:29 -0500

    code to draw the ball and paddles on the screen

--- Pong Tutorial 2D/Assets/GameManager.cs
+++ Pong Tutorial 2D/Assets/GameManager.cs
@@ -7,10 +7,24 @@ public class GameManager : MonoBehaviour
     public Ball ball;
     public Paddle paddle;
 
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Convert screen's pixel coordinate into game's coordinate
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
+        // Create ball
+        Instantiate (ball);
+
+        // Create two paddles 
+        Paddle paddle1 = Instantiate (paddle) as Paddle;
+        Paddle paddle2 = Instantiate (paddle) as Paddle;
+        paddle1.Init (true); // right paddle
+        paddle2.Init (false); // left paddle
     }
 
     // Update is called once per frame
--- Pong Tutorial 2D/Assets/Paddle.cs
+++ Pong Tutorial 2D/Assets/Paddle.cs
@@ -10,6 +10,26 @@ public class Paddle : MonoBehaviour
 
     }
 
+    public void Init(bool isRightPaddle)
+    {
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle) {
+            // Place paddle on the right of screen
+            pos = new Vector2(GameManager.topRight.x, 0);
+            // Move the paddle a bit to the left
+            pos -= Vector2.right * transform.localScale.x;
+        } else {
+            // Place paddle on the left of screen
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            // Move the paddle a bit to the right
+            pos += Vector2.right * transform.localScale.x;
+        }
+
+        // Update the paddle's position
+        transform.position = pos;
+    }
+
     // Update is called once per frame
     void Update()
     {
