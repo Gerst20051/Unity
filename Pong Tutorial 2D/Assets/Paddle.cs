9a7603234b94b846fcacd3bd654474fa48f39ccd
Andrew Gerst <gerst20051@gmail.com>
2018-12-22 04:12:13 -0500

    implement paddle controls and translate logic

--- Pong Tutorial 2D/Assets/Paddle.cs
+++ Pong Tutorial 2D/Assets/Paddle.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    float speed;
+    float height;
+
+    string input;
+    bool isRight;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        height = transform.localScale.y;
+        speed = 5f;
     }
 
     public void Init(bool isRightPaddle)
     {
+        isRight = isRightPaddle;
+
         Vector2 pos = Vector2.zero;
 
         if (isRightPaddle) {
@@ -19,20 +28,29 @@ public class Paddle : MonoBehaviour
             pos = new Vector2(GameManager.topRight.x, 0);
             // Move the paddle a bit to the left
             pos -= Vector2.right * transform.localScale.x;
+
+            input = "PaddleRight";
         } else {
             // Place paddle on the left of screen
             pos = new Vector2(GameManager.bottomLeft.x, 0);
             // Move the paddle a bit to the right
             pos += Vector2.right * transform.localScale.x;
+
+            input = "PaddleLeft";
         }
 
         // Update the paddle's position
         transform.position = pos;
+
+        transform.name = input;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // GetAxis is a number between -1 to 1 (-1 for down, 1 for up)
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
 
+        transform.Translate(move * Vector2.up);
     }
 }
---
+++ Pong Tutorial 2D/README.md
@@ -0,0 +1,3 @@
+Unity Pong Game in 20 Minutes
+
+Video Link: https://www.youtube.com/watch?v=1oY--Zk9b6w
