224c281a8b775446f927b5566282a2d9a7b1e190
Andrew Gerst <gerst20051@gmail.com>
2018-12-23 14:05:05 -0500

    add cylinder object

--- Maze Tutorial 3D/Assets/Scripts/PickUpItem.cs
+++ Maze Tutorial 3D/Assets/Scripts/PickUpItem.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, 4f)) {
+
+        }
     }
 }
