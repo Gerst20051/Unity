1e1e2169afb0ad3976290b0f4ae60743cc65ab6b
Andrew Gerst <gerst20051@gmail.com>
2018-12-22 23:28:34 -0500

    add script to player and rigid body and set y position

--- Maze Tutorial 3D/Assets/Scripts/PlayerMove.cs
+++ Maze Tutorial 3D/Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Input from user
+        if (Input.GetKey("w")) {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("s")) {
+            transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("d")) {
+            transform.position += transform.right * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("a")) {
+            transform.position -= transform.right * Time.deltaTime * moveSpeed;
+        }
     }
 }
