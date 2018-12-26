a5e5d5976415c175bc0b4a91f1fbfcb602c54438
Andrew Gerst <gerst20051@gmail.com>
2018-12-26 16:26:02 -0500

    add force to player

--- Momentum Tutorial 3D/Assets/PlayerMovement.cs
+++ Momentum Tutorial 3D/Assets/PlayerMovement.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // This is a reference to the Rigidbody component called "rb".
+    public Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
+    // We marked this as "Fixed"Update because we
+    // are using it to mess with physics.
+    void FixedUpdate()
     {
-        
+        // Add a force of 2000 on the z-axis.
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }
