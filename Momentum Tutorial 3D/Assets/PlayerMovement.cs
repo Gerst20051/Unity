f83371865ab8475c29e33cfbf0bb9b6e66783442
Andrew Gerst <gerst20051@gmail.com>
2018-12-26 21:58:29 -0500

    adjust some obstacles and add fog to lighting

--- Momentum Tutorial 3D/Assets/PlayerMovement.cs
+++ Momentum Tutorial 3D/Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public float sidewaysForce = 100f;
 
     // We marked this as "Fixed"Update because we
     // are using it to mess with physics.
@@ -16,11 +16,11 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("d")) {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("a")) {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
