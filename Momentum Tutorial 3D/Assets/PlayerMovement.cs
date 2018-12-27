3b7c1887b538cecd01e5a5b18bc5e26162b710c3
Andrew Gerst <gerst20051@gmail.com>
2018-12-26 19:57:48 -0500

    add sideways force to player

--- Momentum Tutorial 3D/Assets/PlayerMovement.cs
+++ Momentum Tutorial 3D/Assets/PlayerMovement.cs
@@ -5,11 +5,22 @@ public class PlayerMovement : MonoBehaviour
     // This is a reference to the Rigidbody component called "rb".
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
     // We marked this as "Fixed"Update because we
     // are using it to mess with physics.
     void FixedUpdate()
     {
-        // Add a force of 2000 on the z-axis.
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        // Add a forward force on the z-axis.
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
